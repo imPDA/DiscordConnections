5ea50af26995650157c4491cee29aac73b5fac52
imPDA <dimaxa7@mail.ru>
2023-12-17 23:22:33 +0300

    0.1.0b1

--- src/discord_connections/__init__.py
+++ src/discord_connections/__init__.py
@@ -2,4 +2,4 @@ from .client import Client
 from .datatypes.metadata import Metadata, MetadataType, MetadataField
 from .datatypes.tokens import DiscordToken
 
-__version__ = '0.1.0b0'
+__version__ = '0.1.0b1'
